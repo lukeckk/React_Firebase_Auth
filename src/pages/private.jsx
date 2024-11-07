acca197d4bcfafd48d622e252bb71a6051452288
Luke Cheng <Lukeecheng@gmail.com>
2024-11-06 23:00:28 -0800

    completed log out function

--- src/pages/private.jsx
+++ src/pages/private.jsx
@@ -1,9 +1,18 @@
 // prevent use from accessing private page wihtout logging in
 
+import { signOut } from "firebase/auth"
+import { auth } from "../firebase"
+
 export const Private = () => {
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => console.log('user signed out'))
+    .catch((error) => console.log(error))
+  }
   return(
     <section>
       <h2>Private page</h2>
+      <button onClick={handleSignOut}>Sign out</button>
     </section>
   )
 }
